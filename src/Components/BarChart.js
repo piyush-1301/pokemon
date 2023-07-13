e303692a434c0d7df0042996c76d1fd8abd5aac3
Piyush Soni <piyush_soni@persistent.com>
2023-07-13 13:48:22 +0530

    Add: error

--- src/Components/BarChart.js
+++ src/Components/BarChart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Bar } from 'react-chartjs-2'
-import 'chart.js/auto'
+// import 'chart.js/auto'
 
 function BarChart({chartData,options}) {
   return <Bar data={chartData} options={options}/>
